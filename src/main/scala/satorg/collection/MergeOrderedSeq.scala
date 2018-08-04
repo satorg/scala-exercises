f22c1413ea60a75a9effb86ca87c5f31bbc30e6d
Sergey Torgashov <satorg@gmail.com>
2018-08-04 23:40:14 +0400

    implementing `mergeOrdered` for `List` (tail-recursive)

--- src/main/scala/satorg/collection/MergeOrderedSeq.scala
+++ src/main/scala/satorg/collection/MergeOrderedSeq.scala
@@ -1,11 +1,12 @@
 package satorg.collection
 
+import scala.annotation.tailrec
+
 object MergeOrderedSeq {
 
   def mergeOrdered[A: Ordering](left: Stream[A], right: Stream[A]): Stream[A] = {
 
     (left, right) match {
-      case (Stream.Empty, Stream.Empty) => Stream.Empty
       case (Stream.Empty, _) => right
       case (_, Stream.Empty) => left
 
@@ -19,5 +20,28 @@ object MergeOrderedSeq {
           rightHead #:: mergeOrdered(left, rightTail)
         }
     }
+  }
+
+  @tailrec
+  private def mergeOrderedR[A: Ordering](left: List[A], right: List[A], acc: List[A]): List[A] = {
+
+    (left, right) match {
+      case (Nil, _) => right reverse_::: acc
+      case (_, Nil) => left reverse_::: acc
+      case (leftHead :: leftTail, rightHead :: rightTail) =>
+        val ordering = implicitly[Ordering[A]]
+
+        if (ordering.lt(leftHead, rightHead)) {
+          mergeOrderedR(leftTail, right, leftHead :: acc)
+        }
+        else {
+          mergeOrderedR(left, rightTail, rightHead :: acc)
+        }
+    }
+  }
+
+  def mergeOrdered[A: Ordering](left: List[A], right: List[A]): List[A] = {
+
+    mergeOrderedR(left, right, Nil).reverse
   }
 }
--- src/test/scala/satorg/collection/MergeOrderedSeqTest.scala
+++ src/test/scala/satorg/collection/MergeOrderedSeqTest.scala
@@ -25,4 +25,22 @@ class MergeOrderedSeqTest extends FreeSpec with Matchers with GeneratorDrivenPro
       actual2 should contain theSameElementsInOrderAs expected
     }
   }
+
+  "mergeOrdered for List should work correctly" in {
+
+    def listGen =
+      Gen.chooseNum(0, 10, 0, 1).
+        flatMap(Gen.listOfN[Int](_, Gen.choose(1, 10))).
+        map(_.sorted)
+
+    forAll((listGen, "left"), (listGen, "right")) { (left, right) =>
+      val expected = (left ::: right).sorted
+
+      val actual1 = mergeOrdered(left, right)
+      val actual2 = mergeOrdered(right, left)
+
+      actual1 should contain theSameElementsInOrderAs expected
+      actual2 should contain theSameElementsInOrderAs expected
+    }
+  }
 }
